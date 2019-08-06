d91cdecfdef48d6028d3a36f25d55b7aa97068c5
Nice <nicenicole.in@gmail.com>
2019-08-06 12:53:06 +0200

    added jQuery in my script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".ordering").hover(function(){
+    $(".ordering").hover(function () {
         $("#pizzaOrders").show();
 
     });
@@ -38,4 +38,75 @@ $(document).ready(function () {
 
             '</div>');
     });
-});
+    function Pizza(firstName, lastName, deliver) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.deliver=deliver
+        this.orders = [];
+    }
+    function Order(size, topping, crust) {
+        this.size = size;
+        this.topping = topping;
+        this.crust = crust;
+    };
+
+    $("form#new-pizza").submit(function (event) {
+        event.preventDefault();
+
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+        var inputtedDeliverMode=$(".new-delivery").val();
+        var newPizza = new Pizza(inputtedFirstName, inputtedLastName, inputtedDeliverMode);
+
+        $(".new-order").each(function () {
+            // var inputtedSize = $(this).find('option:selected').attr("name");
+            var inputtedSize = $(this).find(".new-size").val();
+            var inputtedTopping = $(this).find(".new-topping").val();
+            var inputtedCrust = $(this).find(".new-crust").val();
+            var newOrder = new Order(inputtedSize, inputtedTopping, inputtedCrust)
+            newPizza.orders.push(newOrder)
+        });
+
+        Pizza.prototype.fullName = function () {
+            return this.firstName + " " + this.lastName;
+        }
+        Order.prototype.fullOrder = function () {
+            return this.size + " ," + this.topping + " ," + this.crust;
+        }
+
+
+        $("ul#names").append("<li><span class='order'>" + newPizza.fullName() + "</span></li>");
+        $("ul#deliver").append("<li><span class='order'>" + newPizza.deliver + "</span></li>");
+            newPizza.orders.forEach(function (order) {
+            $("ul#pizzass").append("<li>" + order.fullOrder() +"</li>");
+
+        });
+ 
+
+        $(".pizza").last().click(function () {
+            $("#show-order").show();
+            $("#show-order h2").text(newPizza.fullName());
+            $("#show-order h2").text(newPizza.deliver);
+            // $("ul#pizzass").text("");
+            newPizza.orders.forEach(function (order) {
+                $("ul#pizzass").append("<li>" + newPizza.fullOrder + "</li>");
+
+            });
+            $("#show-order h2").text(newPizza.orders);
+
+
+            });
+        });
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+        $(".new-size").val("");
+        $(".new-topping").val("");
+        $(".new-crust").val("");
+
+    });
+
+
+
+
+
